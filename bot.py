74bec5b591084281e4f29801ea2a053a5f1d187b
Dagar165 <134097703+Dagar165@users.noreply.github.com>
2026-05-13 16:03:16 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -175,7 +175,7 @@ SYSTEM_INSTRUCTION = """
 # Инициализация Gemini
 genai.configure(api_key=GEMINI_KEY)
 gemini_model = genai.GenerativeModel(
-    model_name="models/gemini-1.5-flash",
+    model_name="models/gemini-2.5-flash-lite",
     system_instruction=SYSTEM_INSTRUCTION,
 )
 
